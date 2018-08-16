1d17148e5b429d8767237b99be0f0d469782541b
Standa <stanislav.matas@gmail.com>
2018-08-16 10:34:56 +0200

    Python Crash Course, chapter 19, Django 2.1, second commit

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -13,5 +13,17 @@ urlpatterns = [
     path('topics/', views.topics, name='topics'),
 
     # Detail page for a single topic.
-    path('topics/<int:topic_id>/', views.topic, name='topic')
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+
+    # Page for adding a new topic.
+    path('new_topic/', views.new_topic, name='new_topic'),
+
+    # Page for adding a new entry.
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+
+    # Page for editing en entry.
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
 ]
+
+# ...When a URL matching this pattern is requested, Django sends the request
+# and the ID of the topic to the new_entry() view function.
